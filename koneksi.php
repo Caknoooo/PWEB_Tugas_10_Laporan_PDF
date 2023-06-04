234f8cbb092123e6cf2df1333b1acfc05b18150a
Caknoooo <caknocomel@gmail.com>
2023-06-04 23:49:41 +0700

    feat: create export pdf files

---
+++ koneksi.php
@@ -0,0 +1,9 @@
+<?php
+$host = "localhost"; // Nama hostnya
+$username = "root"; // Username
+$password = ""; // Password (Isi jika menggunakan password)
+$database = "siswa_pdf"; // Nama databasenya
+$connect = mysqli_connect($host, $username, $password, $database);
+
+// Koneksi ke MySQL dengan PDO
+$pdo = new PDO('mysql:host='.$host.';dbname='.$database, $username, $password);
